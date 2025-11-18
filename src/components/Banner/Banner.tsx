9a815492ad0ff15aea335c1a97db75bf826ff8f8
Jeremi Wielewski <wielewski.jeremi@gmail.com>
2025-11-18 08:31:58 +0100

    added banner component

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,9 @@
+export function Banner() {
+  return (
+    <div>
+      <div className="text-blue text-lg leading-1.5 tracking-tight font-medium">
+        Multistep Form
+      </div>
+    </div>
+  );
+}
