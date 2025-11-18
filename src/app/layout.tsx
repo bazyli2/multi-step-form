e3ea5df93a6ef642ea725029083a2ca618be49f3
Jeremi Wielewski <wielewski.jeremi@gmail.com>
2025-11-18 14:12:13 +0100

    added material ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { clsx } from "@/utils/clsx";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "600"] });
 
@@ -17,9 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
-      <body className={clsx(poppins.className, "min-h-full flex flex-col")}>
-        {children}
-      </body>
+      <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+        <body className={clsx(poppins.className, "min-h-full flex flex-col")}>
+          {children}
+        </body>
+      </AppRouterCacheProvider>
     </html>
   );
 }
