6e6d52634cffd95f45e044f3a6037b24c0f53e41
Jeremi Wielewski <wielewski.jeremi@gmail.com>
2025-11-18 09:02:13 +0100

    added full height in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { clsx } from "@/utils/clsx";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500"] });
 
@@ -15,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={poppins.className}>{children}</body>
+    <html lang="en" className="h-full">
+      <body className={clsx(poppins.className, "min-h-full")}>{children}</body>
     </html>
   );
 }
