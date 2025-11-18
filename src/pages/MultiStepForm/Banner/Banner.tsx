ad1dcaf28abcdee02fbe4b83fbe429347dc477fb
Jeremi Wielewski <wielewski.jeremi@gmail.com>
2025-11-18 09:18:02 +0100

    removed banner component

--- src/pages/MultiStepForm/Banner/Banner.tsx
+++
@@ -1,9 +0,0 @@
-export function Banner() {
-  return (
-    <div>
-      <div className="text-blue text-lg leading-6 tracking-tight font-medium">
-        Multistep Form
-      </div>
-    </div>
-  );
-}
--- src/pages/MultiStepForm/MultiStepForm.tsx
+++ src/pages/MultiStepForm/MultiStepForm.tsx
@@ -1,10 +1,10 @@
-import { Banner } from "./Banner";
-
 export function MultiStepForm() {
   return (
     <div className="flex-1 bg-blue flex items-center justify-center">
       <div className="max-w-7xl bg-white flex-1 py-16 flex flex-col items-center">
-        <Banner />
+        <div className="text-blue text-lg leading-6 tracking-tight font-medium">
+          Multistep Form
+        </div>
       </div>
     </div>
   );
