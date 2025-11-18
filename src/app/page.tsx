e6ea4a1abd0d153db80676f923a1050995058690
Jeremi Wielewski <wielewski.jeremi@gmail.com>
2025-11-18 08:34:30 +0100

    added multi step form page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import { Banner } from "@/components/Banner";
+import { MultiStepForm } from "@/pages/MultiStepForm/MultiStepForm";
 
-export default function Home() {
-  return (
-    <div>
-      <Banner />
-    </div>
-  );
-}
+export default MultiStepForm;
---
+++ src/pages/MultiStepForm/MultiStepForm.tsx
@@ -0,0 +1,9 @@
+import { Banner } from "@/components/Banner";
+
+export function MultiStepForm() {
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
+}
