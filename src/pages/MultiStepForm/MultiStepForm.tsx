3a3840dc7eb44a082ae9cf43411961f241f520c3
Jeremi Wielewski <wielewski.jeremi@gmail.com>
2025-11-19 11:41:18 +0100

    added semantic tags

--- src/pages/MultiStepForm/MultiStepForm.tsx
+++ src/pages/MultiStepForm/MultiStepForm.tsx
@@ -2,30 +2,30 @@ import { FormStepper } from "./FormStepper";
 
 export function MultiStepForm() {
   return (
-    <div className="flex-1 bg-blue flex items-center justify-center tracking-tight">
-      <div className="max-w-7xl bg-white flex-1 py-16 flex flex-col items-center">
-        <div className="text-blue text-lg leading-6 font-medium mb-8">
-          Multistep Form
-        </div>
-        <div className="font-semibold text-6xl leading-20 min-h-24 flex items-center mb-12">
-          <div className="px-8">Give us your</div>
-          <div className="px-4 bg-blue text-white">feedback</div>
-        </div>
-        <div className="max-w-lg w-full flex flex-col gap-8">
+    <main className="flex-1 bg-blue flex items-center justify-center tracking-tight">
+      <section className="max-w-7xl bg-white flex-1 py-16 flex flex-col items-center">
+        <header className="text-center mb-12">
+          <h1 className="text-blue text-lg leading-6 font-medium mb-8">
+            Multistep Form
+          </h1>
+          <h2 className="font-semibold text-6xl leading-20 min-h-24 flex items-center">
+            <span className="px-8">Give us your</span>
+            <span className="px-4 bg-blue text-white">feedback</span>
+          </h2>
+        </header>
+        <section className="max-w-lg w-full flex flex-col gap-8">
           <FormStepper />
-          <div className="flex flex-col gap-2">
-            <div className="text-blue font-medium text-sm leading-6">
-              Step 1/4
-            </div>
-            <div className="font-medium text-xl leading-7">
+          <header className="flex flex-col gap-2">
+            <p className="text-blue font-medium text-sm leading-6">Step 1/4</p>
+            <h3 className="font-medium text-xl leading-7">
               Enter your Name and Email to Begin!
-            </div>
-            <div className="text-arsenic leading-7">
+            </h3>
+            <p className="text-arsenic leading-7">
               Lorem ipsum is placeholder text.
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+            </p>
+          </header>
+        </section>
+      </section>
+    </main>
   );
 }
