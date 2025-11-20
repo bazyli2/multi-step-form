670af0d2f4c88bece1f8d5c958ad75c95bf39859
Jeremi Wielewski <wielewski.jeremi@gmail.com>
2025-11-20 17:00:23 +0100

    added header to step 2 component

--- src/pages/MultiStepForm/steps/Step2/Step2.tsx
+++ src/pages/MultiStepForm/steps/Step2/Step2.tsx
@@ -1,3 +1,15 @@
 export function Step2() {
-  return <></>;
+  return (
+    <>
+      <header className="flex flex-col gap-2">
+        <p className="text-blue font-medium text-sm leading-6">Step 2/4</p>
+        <h3 className="font-medium text-xl leading-7">
+          Which feature are you most interested in?
+        </h3>
+        <p className="text-arsenic leading-7">
+          Lorem ipsum is placeholder text.
+        </p>
+      </header>
+    </>
+  );
 }
