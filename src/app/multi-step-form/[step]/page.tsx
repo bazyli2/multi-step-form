95012e05be227326067120508710d4c1f14072e0
Jeremi Wielewski <wielewski.jeremi@gmail.com>
2025-11-24 10:55:08 +0100

    added router component for multi step form

--- src/app/multi-step-form/[step]/page.tsx
+++ src/app/multi-step-form/[step]/page.tsx
@@ -1,23 +1,3 @@
-import { paramsSchema } from "@/modules/multi-step-form/schema";
-import { Feedback } from "@/pages/Feedback";
-import { LocationDetails } from "@/pages/LocationDetails";
-import { PersonalDetails } from "@/pages/PersonalDetails";
-import { parseParams } from "@/utils/parseParams";
+import { Router } from "@/modules/multi-step-form/Router";
 
-export default async function Page(props: Props) {
-  const params = await props.params;
-  const { step } = parseParams(paramsSchema, params);
-  if (step === 1) {
-    return <PersonalDetails />;
-  }
-  if (step === 2) {
-    return <Feedback />;
-  }
-  if (step === 3) {
-    return <LocationDetails />;
-  }
-}
-
-interface Props {
-  params: Promise<unknown>;
-}
+export default Router;
