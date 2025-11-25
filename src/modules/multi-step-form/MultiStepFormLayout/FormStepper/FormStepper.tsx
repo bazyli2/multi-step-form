858a0eb5e3e048438722d876f6daa5995fd2619b
Jeremi Wielewski <wielewski.jeremi@gmail.com>
2025-11-25 17:46:18 +0100

    added displaying current step in stepper

--- src/modules/multi-step-form/MultiStepFormLayout/FormStepper/FormStepper.tsx
+++ src/modules/multi-step-form/MultiStepFormLayout/FormStepper/FormStepper.tsx
@@ -5,13 +5,14 @@ import { useState } from "react";
 import { STEP_COUNT } from "../../config";
 import { StepConnector } from "./StepConnector";
 import { StepIcon } from "./StepIcon";
+import { useStepParam } from "../../hooks/useStepParam";
 
 const steps = Array.from({ length: STEP_COUNT }, (v, i) => i);
 
 export function FormStepper() {
-  const [activeStep, setActiveStep] = useState(0);
+  const step = useStepParam();
   return (
-    <Stepper connector={<StepConnector />} activeStep={activeStep}>
+    <Stepper connector={<StepConnector />} activeStep={step - 1}>
       {steps.map((step) => (
         <Step key={step}>
           <StepLabel StepIconComponent={StepIcon} />
