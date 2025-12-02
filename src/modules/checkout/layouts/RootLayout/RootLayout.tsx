9f95f5115ad91d8ab331c306f3a6d267bb22abaf
Jeremi Wielewski <wielewski.jeremi@gmail.com>
2025-12-02 16:40:17 +0100

    added schema to checkout

--- src/modules/checkout/layouts/RootLayout/RootLayout.tsx
+++ src/modules/checkout/layouts/RootLayout/RootLayout.tsx
@@ -1,10 +1,4 @@
-import {
-  Box,
-  Card,
-  CardContent,
-  Grid,
-  Typography,
-} from "@mui/material";
+import { Box, Card, CardContent, Grid, Typography } from "@mui/material";
 import ColorModeIconDropdown from "./ColorModeIconDropdown";
 import SitemarkIcon from "./SitemarkIcon";
 import Info from "./Info";
---
+++ src/modules/checkout/schema.ts
@@ -0,0 +1,9 @@
+import z from "zod";
+
+export const paramsSchema = z.object({
+  step: z.union([
+    z.literal("1").transform(() => 1 as const),
+    z.literal("2").transform(() => 2 as const),
+    z.literal("3").transform(() => 3 as const),
+  ]),
+});
