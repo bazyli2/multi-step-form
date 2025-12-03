3f68b615361feae547bb366c5fc7d38df5705e92
Jeremi Wielewski <wielewski.jeremi@gmail.com>
2025-12-03 10:57:11 +0100

    added circular progress to loading component

--- src/modules/checkout/Loading/Loading.tsx
+++ src/modules/checkout/Loading/Loading.tsx
@@ -1,3 +1,9 @@
+import { CircularProgress } from "@mui/material";
+
 export function Loading() {
-  return <div>loading...</div>;
+  return (
+    <div className="w-full flex-1 flex items-center justify-center">
+      <CircularProgress />
+    </div>
+  );
 }
