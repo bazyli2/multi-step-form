1cc0c044379be04fd09abd4da7fe7487fe549f4d
Jeremi Wielewski <wielewski.jeremi@gmail.com>
2025-12-03 09:40:17 +0100

    added ssg to checkout pages

--- src/app/checkout/[step]/page.ts
+++ src/app/checkout/[step]/page.ts
@@ -1,3 +1,7 @@
 import { Router } from "@/modules/checkout/Router";
 
+export async function generateStaticParams() {
+  return [{ step: "1" }, { step: "2" }, { step: "3" }, { step: "4" }];
+}
+
 export default Router;
