bc5045ffb23914d3b304884b64eed3a74e19b49f
Jeremi Wielewski <wielewski.jeremi@gmail.com>
2025-12-04 22:33:21 +0100

    added FormField component to ShippingAddress page

--- src/modules/checkout/pages/ShippingAddress/ShippingAddress.tsx
+++ src/modules/checkout/pages/ShippingAddress/ShippingAddress.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { FormField } from "@/components/FormField";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormLabel from "@mui/material/FormLabel";
@@ -15,119 +16,89 @@ const FormGrid = styled(Grid)(() => ({
 export function ShippingAddress() {
   return (
     <Grid container spacing={3}>
-      <FormGrid size={{ xs: 12, md: 6 }}>
-        <FormLabel htmlFor="first-name" required>
-          First name
-        </FormLabel>
-        <OutlinedInput
-          id="first-name"
-          name="first-name"
-          type="name"
-          placeholder="John"
-          autoComplete="first name"
-          required
-          size="small"
-        />
-      </FormGrid>
-      <FormGrid size={{ xs: 12, md: 6 }}>
-        <FormLabel htmlFor="last-name" required>
-          Last name
-        </FormLabel>
-        <OutlinedInput
-          id="last-name"
-          name="last-name"
-          type="last-name"
-          placeholder="Snow"
-          autoComplete="last name"
-          required
-          size="small"
-        />
-      </FormGrid>
-      <FormGrid size={{ xs: 12 }}>
-        <FormLabel htmlFor="address1" required>
-          Address line 1
-        </FormLabel>
-        <OutlinedInput
-          id="address1"
-          name="address1"
-          type="address1"
-          placeholder="Street name and number"
-          autoComplete="shipping address-line1"
-          required
-          size="small"
-        />
-      </FormGrid>
-      <FormGrid size={{ xs: 12 }}>
-        <FormLabel htmlFor="address2">Address line 2</FormLabel>
-        <OutlinedInput
-          id="address2"
-          name="address2"
-          type="address2"
-          placeholder="Apartment, suite, unit, etc. (optional)"
-          autoComplete="shipping address-line2"
-          required
-          size="small"
-        />
-      </FormGrid>
-      <FormGrid size={{ xs: 6 }}>
-        <FormLabel htmlFor="city" required>
-          City
-        </FormLabel>
-        <OutlinedInput
-          id="city"
-          name="city"
-          type="city"
-          placeholder="New York"
-          autoComplete="City"
-          required
-          size="small"
-        />
-      </FormGrid>
-      <FormGrid size={{ xs: 6 }}>
-        <FormLabel htmlFor="state" required>
-          State
-        </FormLabel>
-        <OutlinedInput
-          id="state"
-          name="state"
-          type="state"
-          placeholder="NY"
-          autoComplete="State"
-          required
-          size="small"
-        />
-      </FormGrid>
-      <FormGrid size={{ xs: 6 }}>
-        <FormLabel htmlFor="zip" required>
-          Zip / Postal code
-        </FormLabel>
-        <OutlinedInput
-          id="zip"
-          name="zip"
-          type="zip"
-          placeholder="12345"
-          autoComplete="shipping postal-code"
-          required
-          size="small"
-        />
-      </FormGrid>
-      <FormGrid size={{ xs: 6 }}>
-        <FormLabel htmlFor="country" required>
-          Country
-        </FormLabel>
-        <OutlinedInput
-          id="country"
-          name="country"
-          type="country"
-          placeholder="United States"
-          autoComplete="shipping country"
-          required
-          size="small"
-        />
-      </FormGrid>
+      <FormField
+        required
+        label="First Name"
+        gridProps={{ size: { xs: 12, md: 6 } }}
+        name="first-name"
+        type="name"
+        placeholder="John"
+        autoComplete="first name"
+        size="small"
+      />
+      <FormField
+        gridProps={{ size: { xs: 12, md: 6 } }}
+        label="Last name"
+        name="last-name"
+        type="last-name"
+        placeholder="Snow"
+        autoComplete="last name"
+        required
+        size="small"
+      />
+      <FormField
+        gridProps={{ size: { xs: 12 } }}
+        label="Address line 1"
+        name="address1"
+        type="address1"
+        placeholder="Street name and number"
+        autoComplete="shipping address-line1"
+        required
+        size="small"
+      />
+      <FormField
+        label="Address line 2"
+        gridProps={{ size: { xs: 12 } }}
+        name="address2"
+        type="address2"
+        placeholder="Apartment, suite, unit, etc. (optional)"
+        autoComplete="shipping address-line2"
+        required
+        size="small"
+      />
+      <FormField
+        label="City"
+        gridProps={{ size: { xs: 6 } }}
+        name="city"
+        type="city"
+        placeholder="New York"
+        autoComplete="City"
+        required
+        size="small"
+      />
+      <FormField
+        label="State"
+        gridProps={{ size: { xs: 6 } }}
+        name="state"
+        type="state"
+        placeholder="NY"
+        autoComplete="State"
+        required
+        size="small"
+      />
+      <FormField
+        label="Zip / Postal code"
+        gridProps={{ size: { xs: 6 } }}
+        name="zip"
+        type="zip"
+        placeholder="12345"
+        autoComplete="shipping postal-code"
+        required
+        size="small"
+      />
+      <FormField
+        label="Country"
+        gridProps={{ size: { xs: 6 } }}
+        name="country"
+        type="country"
+        placeholder="United States"
+        autoComplete="shipping country"
+        required
+        size="small"
+      />
       <FormGrid size={{ xs: 12 }}>
         <FormControlLabel
-          control={<Checkbox name="saveAddress" value="yes" />}
+          control={<Checkbox name="saveAddress" />}
           label="Use this address for payment details"
         />
       </FormGrid>
