9b7b524e047b2c798fa1b914eb7587937569ab8a
Jeremi Wielewski <wielewski.jeremi@gmail.com>
2025-12-08 10:20:00 +0100

    added react hook form to PaymentForm

--- src/modules/checkout/pages/PaymentDetails/PaymentForm/PaymentForm.tsx
+++ src/modules/checkout/pages/PaymentDetails/PaymentForm/PaymentForm.tsx
@@ -12,13 +12,33 @@ import { withMask } from "use-mask-input";
 import { styled } from "@mui/material/styles";
 import { Grid } from "@mui/material";
 import { PaymentContainer } from "./PaymentContainer";
+import z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { RHFMaskFormField } from "@/components/RHFMaskFormField";
+import { RHFFormField } from "@/components/RHFFormField";
 
 const FormGrid = styled(Grid)(() => ({
   display: "flex",
   flexDirection: "column",
 }));
 
+const formSchema = z.object({
+  cardNumber: z.string(),
+  cvv: z.string(),
+  name: z.string(),
+  expirationDate: z.string(),
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
 export function PaymentForm() {
+  const {
+    register,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(formSchema),
+  });
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
       <PaymentContainer>
@@ -41,59 +61,55 @@ export function PaymentForm() {
             gap: 2,
           }}
         >
-          <FormGrid sx={{ flexGrow: 1 }}>
-            <FormLabel htmlFor="card-number" required>
-              Card number
-            </FormLabel>
-            <OutlinedInput
-              id="card-number"
-              autoComplete="card-number"
-              placeholder="0000 0000 0000 0000"
-              required
-              size="small"
-              inputRef={withMask("9999 9999 9999 9999")}
-            />
-          </FormGrid>
-          <FormGrid sx={{ maxWidth: "20%" }}>
-            <FormLabel htmlFor="cvv" required>
-              CVV
-            </FormLabel>
-            <OutlinedInput
-              id="cvv"
-              autoComplete="CVV"
-              placeholder="123"
-              required
-              size="small"
-              inputRef={withMask("999")}
-            />
-          </FormGrid>
+          <RHFMaskFormField
+            register={register}
+            error={errors.cardNumber}
+            label="Card Number"
+            gridProps={{ sx: { flexGrow: 1 } }}
+            name="cardNumber"
+            autoComplete="card-number"
+            placeholder="0000 0000 0000 0000"
+            required
+            size="small"
+            mask="9999 9999 9999 9999"
+          />
+          <RHFMaskFormField
+            register={register}
+            error={errors.cvv}
+            name="cvv"
+            label="CVV"
+            gridProps={{ sx: { maxWidth: "20%" } }}
+            autoComplete="CVV"
+            placeholder="123"
+            required
+            size="small"
+            mask="999"
+          />
         </Box>
         <Box sx={{ display: "flex", gap: 2 }}>
-          <FormGrid sx={{ flexGrow: 1 }}>
-            <FormLabel htmlFor="card-name" required>
-              Name
-            </FormLabel>
-            <OutlinedInput
-              id="card-name"
-              autoComplete="card-name"
-              placeholder="John Smith"
-              required
-              size="small"
-            />
-          </FormGrid>
-          <FormGrid sx={{ flexGrow: 1 }}>
-            <FormLabel htmlFor="card-expiration" required>
-              Expiration date
-            </FormLabel>
-            <OutlinedInput
-              id="card-expiration"
-              autoComplete="card-expiration"
-              placeholder="MM/YY"
-              required
-              size="small"
-              inputRef={withMask("99/99")}
-            />
-          </FormGrid>
+          <RHFFormField
+            gridProps={{ sx: { flexGrow: 1 } }}
+            register={register}
+            error={errors.name}
+            name="name"
+            label="Name"
+            autoComplete="card-name"
+            placeholder="John Smith"
+            required
+            size="small"
+          />
+          <RHFMaskFormField
+            gridProps={{ sx: { flexGrow: 1 } }}
+            register={register}
+            error={errors.expirationDate}
+            name="expirationDate"
+            label="Expiration date"
+            autoComplete="card-expiration"
+            placeholder="MM/YY"
+            required
+            size="small"
+            mask="99/99"
+          />
         </Box>
       </PaymentContainer>
       <FormControlLabel
