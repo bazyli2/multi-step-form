3dc152579d91e145662d4ce165d845ac515ba5cd
Jeremi Wielewski <wielewski.jeremi@gmail.com>
2025-12-10 16:52:32 +0100

    added error test case

--- src/components/RHFFormField/RHFFormField.test.tsx
+++ src/components/RHFFormField/RHFFormField.test.tsx
@@ -1,30 +1,36 @@
 import "@testing-library/jest-dom";
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { RHFFormField } from "./RHFFormField";
 
 const formSchema = z.object({
-  name: z.string(),
+  name: z.string().min(3),
 });
 
 function TestForm() {
   const {
     register,
     formState: { errors },
+    handleSubmit,
   } = useForm({
     resolver: zodResolver(formSchema),
   });
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <RHFFormField
         register={register}
         name="name"
         error={errors.name}
         label="Name"
       />
+      <button type="submit">button</button>
     </form>
   );
 }
@@ -32,5 +38,8 @@ function TestForm() {
 test("RHFFieldForm", async () => {
   render(<TestForm />);
   const input = await screen.findByLabelText("Name");
-  expect(input).toBeInTheDocument();
+  const submitButton = await screen.findByRole("button");
+  await userEvent.type(input, "a");
+  await userEvent.click(submitButton);
+  expect(screen.getByText(/Too\ Small/i)).toBeInTheDocument();
 });
